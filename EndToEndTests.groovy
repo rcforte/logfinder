c995133ccbfce990de56d30b85dea675c8895420
Rafael Forte <rcforte@gmail.com>
2017-09-20 01:04:18 +0100

    Reformatting and removing unused test

--- EndToEndTests.groovy
+++ EndToEndTests.groovy
@@ -1,17 +1,27 @@
 class EndToEndTests extends GroovyTestCase {
-    void "test logfinder finds 1 word in 1 file"() {
-        def cmd = [ 'java', '-jar', 'build/libs/logfinder-java-all.jar', '-path', '.', '-extensions', '*.txt', 'Rafael' ]
-        def proc = cmd.execute()
-        def output = []
-        proc.text.eachLine { output << it }
-        assert '.\\test.txt' in output
-    }
+  void "test logfinder finds 1 word in 1 file"() {
+    def cmd = [ 
+      'java', '-jar', 'build/libs/logfinder-java-all.jar', 
+      '-path', '.', 
+      '-extensions', '*.txt', 
+      'Rafael' 
+    ]
+    def proc = cmd.execute()
+    def output = []
+    proc.text.eachLine { output << it }
+    assert '.\\test.txt' in output
+  }
 
-    void "test logfinder finds 2 words in a file using 'and' semantics"() {
-        def cmd = [ 'java', '-jar', 'build/libs/logfinder-java-all.jar', '-path', '.', '-and', '-extensions', '*.txt', 'Rafael', 'Lee']
-        def proc = cmd.execute()
-        def output = []
-        proc.text.eachLine { output << it }
-        assert output.size == 0
-    }
+  void "test logfinder finds 2 words in a file using 'and' semantics"() {
+    def cmd = [ 'java', '-jar', 'build/libs/logfinder-java-all.jar', 
+      '-path', '.', 
+      '-and', 
+      '-extensions', '*.txt', 
+      'Rafael', 'Lee'
+    ]
+    def proc = cmd.execute()
+    def output = []
+    proc.text.eachLine { output << it }
+    assert output.size == 0
+  }
 }
